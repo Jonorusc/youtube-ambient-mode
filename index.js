3e02ed5a85fd198ab563b49fab8eb60221f159e5
João Lucas <joaolucascp16@gmail.com>
2023-06-06 14:02:03 -0300

    update: using canvas instead of div 'video-container'

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let video = null
 const videoContainer = document.querySelector(".video-container")
+let canvasGradient = document.createElement("canvas")
+canvasGradient.classList.add("canvas-gradient")
 
 document.getElementById("video-input").addEventListener("change", function (event) {
   const file = event.target.files[0]
@@ -23,6 +25,7 @@ document.getElementById("video-input").addEventListener("change", function (even
 
       const removeVideoBtn = videoTitle.querySelector(".remove")
 
+      // set videoContainer size to video size
       const aspectRatio = this.videoWidth / this.videoHeight
       const containerWidth = videoContainer.offsetWidth
       const containerHeight = Math.min(555, containerWidth / aspectRatio)
@@ -33,6 +36,8 @@ document.getElementById("video-input").addEventListener("change", function (even
       videoContainerSkeleton.style.display = "none"
 
       videoContainer.appendChild(this)
+      // append canvas to video container
+      canvasBackground()
 
       removeVideoBtn.style.display = "block"
 
@@ -45,115 +50,164 @@ document.getElementById("video-input").addEventListener("change", function (even
         videoTitleSkeleton.style.display = "block"
         videoTitle.classList.remove("loaded")
         videoTitle.removeChild(title)
-        removeBackgound()
+        canvasBackground(false)
       })
 
-      getVideoColours(this)
+      this.addEventListener("ended", () => canvasBackground(false))
+    })
 
-      this.addEventListener("ended", () => removeBackgound())
+    video.addEventListener("play", function () {
+      // append canvas to video container
+      canvasBackground()
+
+      const canvas = document.createElement("canvas")
+      const context = canvas.getContext("2d", { willReadFrequently: true })
+      const interval = 900
+
+      setInterval(function () {
+        if (video.paused || video.ended) return
+
+        context.drawImage(video, 0, 0, canvas.width, canvas.height)
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+        const colours = getDominantColours(imageData.data, canvas)
+        updateBackground(true, colours)
+      }, interval)
+    })
+
+    video.addEventListener("pause", function () {
+      updateBackground(false)
     })
   }
 })
 
-function removeBackgound() {
-  videoContainer.style.backgroundImage = "none"
-  videoContainer.style.boxShadow = "none"
-}
-
-function getVideoColours(video) {
-  const canvas = document.createElement("canvas")
-  const context = canvas.getContext("2d", { willReadFrequently: true })
-  const interval = 1300
-
-  video.addEventListener("play", function () {
-    setInterval(function () {
-      if (video.paused || video.ended) return
-
-      context.drawImage(video, 0, 0, canvas.width, canvas.height)
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
-      const colours = getDominantColours(imageData.data, canvas)
-      updateBackground(colours)
-    }, interval)
-  })
-}
-
-function updateBackground(dominantColours) {
-  if (dominantColours.length > 0) {
-    const gradientStops = dominantColours.map((color) => {
-      const position = color.position
-      const rgbaColor = `rgba(${color.rgb}, 0.45)`
-      return `radial-gradient(ellipse at ${position}, ${rgbaColor}, transparent)`
-    })
-
-    const radialGradient = gradientStops.join(", ")
-
-    videoContainer.style.backgroundImage = radialGradient
-    const topOrBottom = (index) => (dominantColours[index].position === "top" ? "-39px" : dominantColours[index].position === "bottom" ? "39px" : "0")
-
-    const boxShadow = `
-    -4px ${topOrBottom(0)} 40px 20px rgba(${dominantColours[0].rgb},0.35),
-    -4px ${topOrBottom(1)} 40px 20px rgba(${dominantColours[1].rgb},0.35),
-    -32px 0px 30px 25px rgba(${dominantColours[2].rgb},0.35)`
-
-    videoContainer.style.boxShadow = boxShadow
-  } else {
-    videoContainer.style.backgroundImage = "none"
-    videoContainer.style.boxShadow = "none"
+function canvasBackground(action = true) {
+  if (action && !videoContainer.querySelector(".canvas-gradient")) {
+    videoContainer.appendChild(canvasGradient)
+  } else if(!action && videoContainer.querySelector(".canvas-gradient")){
+    videoContainer.removeChild(canvasGradient)
   }
 }
 
-function getDominantColours(imageData, canvas) {
+function updateBackground(update, dominantColours = []) {
+  const ctx = canvasGradient.getContext("2d")
+
+  if (!update) return
+
+  try {
+    const sortedColours = dominantColours.sort((a, b) => {
+      if (a.position === b.position) {
+        return a.count > b.count ? -1 : 1
+      }
+      if (a.position === "left") return -1
+      if (b.position === "left") return 1
+      if (a.position === "top" && b.position === "right") return -1
+      if (a.position === "right" && b.position === "top") return 1
+      if (a.position === "top" && b.position === "bottom") return -1
+      if (a.position === "bottom" && b.position === "top") return 1
+      if (a.position === "right" && b.position === "bottom") return -1
+      if (a.position === "bottom" && b.position === "right") return 1
+      return 0
+    })
+
+    sortedColours.forEach((colour) => {
+      let gradient
+
+      switch (colour.position) {
+        case "left":
+          gradient = ctx.createLinearGradient(0, 0, canvasGradient.width, 0)
+          break
+        case "top":
+          gradient = ctx.createLinearGradient(0, 0, 0, canvasGradient.height)
+          break
+        case "right":
+          gradient = ctx.createLinearGradient(canvasGradient.width, 0, 0, 0)
+          break
+        case "bottom":
+          gradient = ctx.createLinearGradient(0, canvasGradient.height, 0, 0)
+          break
+        default:
+          throw new Error("Invalid position colour: " + colour.position)
+      }
+
+      gradient.addColorStop(0, colour.rgba)
+      gradient.addColorStop(1, "transparent")
+
+      ctx.fillStyle = gradient
+      ctx.fillRect(0, 0, canvasGradient.width, canvasGradient.height)
+
+      const boxShadow =
+        colour.position === "left"
+          ? "-15px 0px 30px " + colour.rgba
+          : colour.position === "top"
+          ? "0px -15px 30px " + colour.rgba
+          : colour.position === "right"
+          ? "15px 0px 30px " + colour.rgba
+          : "0px 15px 30px " + colour.rgba
+
+      canvasGradient.style.boxShadow = canvasGradient.style.boxShadow ? canvasGradient.style.boxShadow + "," + boxShadow : boxShadow
+    })
+  } catch (error) {
+    throw new Error("An error occurred while trying to create the gradient colours: " + error)
+  }
+}
+
+function getDominantColours(imageData, canvas, opacity = 0.15) {
   let colourCounts = {}
+  let colours = []
   const pixels = imageData.length / 4
   const videoWidth = canvas.width
   const videoHeight = canvas.height
 
-  for (let i = 0; i < pixels; i++) {
-    const r = imageData[i * 4]
-    const g = imageData[i * 4 + 1]
-    const b = imageData[i * 4 + 2]
-    const rgb = r + "," + g + "," + b
+  try {
+    for (let i = 0; i < pixels; i++) {
+      const r = imageData[i * 4]
+      const g = imageData[i * 4 + 1]
+      const b = imageData[i * 4 + 2]
+      const rgba = r + "," + g + "," + b + "," + opacity
 
-    if (colourCounts[rgb]) {
-      colourCounts[rgb].count += 1
-    } else {
-      colourCounts[rgb] = {
-        count: 1,
-        position: null,
+      if (colourCounts[rgba]) {
+        colourCounts[rgba].count += 1
+      } else {
+        colourCounts[rgba] = {
+          count: 1,
+          position: null,
+        }
+      }
+
+      const row = Math.floor(i / videoWidth)
+      const col = i % videoWidth
+
+      const distanceToTop = row
+      const distanceToBottom = videoHeight - row - 1
+      const distanceToLeft = col
+      const distanceToRight = videoWidth - col - 1
+
+      if (distanceToTop <= distanceToBottom && distanceToTop <= distanceToLeft && distanceToTop <= distanceToRight) {
+        colourCounts[rgba].position = "top"
+      } else if (distanceToBottom <= distanceToTop && distanceToBottom <= distanceToLeft && distanceToBottom <= distanceToRight) {
+        colourCounts[rgba].position = "bottom"
+      } else if (distanceToLeft <= distanceToTop && distanceToLeft <= distanceToBottom && distanceToLeft <= distanceToRight) {
+        colourCounts[rgba].position = "left"
+      } else {
+        colourCounts[rgba].position = "right"
       }
     }
 
-    const row = Math.floor(i / videoWidth)
-    const col = i % videoWidth
-
-    const distanceToTop = row
-    const distanceToBottom = videoHeight - row - 1
-    const distanceToLeft = col
-    const distanceToRight = videoWidth - col - 1
-
-    if (distanceToTop <= distanceToBottom && distanceToTop <= distanceToLeft && distanceToTop <= distanceToRight) {
-      colourCounts[rgb].position = "top"
-    } else if (distanceToBottom <= distanceToTop && distanceToBottom <= distanceToLeft && distanceToBottom <= distanceToRight) {
-      colourCounts[rgb].position = "bottom"
-    } else if (distanceToLeft <= distanceToTop && distanceToLeft <= distanceToBottom && distanceToLeft <= distanceToRight) {
-      colourCounts[rgb].position = "left"
-    } else {
-      colourCounts[rgb].position = "right"
+    for (let colour in colourCounts) {
+      colours.push({
+        rgba: `rgba(${colour})`,
+        count: colourCounts[colour].count,
+        position: colourCounts[colour].position,
+      })
     }
-  }
 
-  let colours = []
-  for (let colour in colourCounts) {
-    colours.push({
-      rgb: colour,
-      count: colourCounts[colour].count,
-      position: colourCounts[colour].position,
+    colours.sort(function (a, b) {
+      return b.count - a.count
     })
+  } catch (error) {
+    throw new Error("An error occurred while trying to select dominant colours: " + error)
   }
 
-  colours.sort(function (a, b) {
-    return b.count - a.count
-  })
-
+  // returning the top 3 colours
   return colours.slice(0, 3)
 }
